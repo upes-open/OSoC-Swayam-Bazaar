4f5612fb832e095fabda915afb3a358336af1cab
rishabh-108272 <rishitibest123@gmail.com>
2023-11-22 00:34:23 +0530

    Updated Feed

---
+++ Swayam-Bazaar-Application/src/components/groceriesData.jsx
@@ -0,0 +1,191 @@
+import RiceImage from"./images/Rice.jpg";
+import SoyBeans from "./images/Soybeans.jpg";
+import Tomato from "./images/Tomatoes.jpg";
+import Raymond from "./images/Logo_.png";
+
+
+const groceriesData = [
+    {
+        id: 1,
+        image: RiceImage,
+        name: 'Basmati Rice',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '08:00 AM',
+        closingTime: '08:00 PM',
+        button:'click'
+    },
+    {
+        id: 2,
+        image: SoyBeans,
+        name: 'Raw Soybeans',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 3,
+        image:Tomato,
+        name: 'Fresh Tomatoes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 4,
+        image:Tomato,
+        name: 'Fresh Tomatoes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 5,
+        image:Tomato,
+        name: 'Fresh Tomatoes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 6,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 7,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 8,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 9,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 10,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 11,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 12,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 13,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 14,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 15,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 16,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 17,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 18,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 19,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    },
+    {
+        id: 20,
+        image:Raymond,
+        name: 'Brand Clothes',
+        Address:'Lorem-Ipsum Dolor-715245',
+        openingTime: '07:00 AM',
+        closingTime: '09:00 PM',
+        button:'click'
+    }
+    
+];
+
+export default groceriesData;
