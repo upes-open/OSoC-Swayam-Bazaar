52742e36d8b6ce7d5bac0bd0a78894e49a2ad816
rishabh-108272 <rishitibest123@gmail.com>
2024-04-14 17:17:04 +0530

    Updated cart

--- Swayam-Bazaar-Application/src/App.js
+++ Swayam-Bazaar-Application/src/App.js
@@ -9,10 +9,11 @@ import Navbar from './components/navbar/Navbar';
 import Feedback from './components/Feedbacks/feedback.js';
 import Feed from './components/Feedbacks/allfeed.js';
 import ContactUs from './components/ContactUs/Contact.js';
-import Cart from "./components/cart/Functionality.js";
 import Dashboard from "./components/dashboard/dash";
 import Products from "./components/dashboard/Products";
 import Order from "./components/dashboard/Order";
+import Cart from "./components/cart/Functionality.js";
+// import Basket from "./components/cart/Basket.js";
 function App() {
   return (
     <>
@@ -27,8 +28,8 @@ function App() {
           <Route path='/feedback' element={<Feedback />}></Route>
           <Route path='/feeds' element={<Feed />}></Route>
           <Route path='/allproducts' element={<Cart />}></Route>
+          {/* <Route path='/bucket' element={<Basket />}></Route> */}
           <Route path='/contactus' element={<ContactUs />}></Route>
-          <Route path='/cart' element={<Cart />}></Route>
           <Route path='/' element={<Dashboard />} >
             <Route path='/dashboard' element={<Dashboard />}></Route>
             <Route path='/Products' element={<Products />}></Route>
